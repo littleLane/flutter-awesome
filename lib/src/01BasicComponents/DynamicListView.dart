f32acee49578824e22f7f371d54bc3484c8e3f0a
littleLane <lzz857183384@163.com>
2020-09-06 13:21:24 +0800

    feat: ListView

---
+++ lib/src/01BasicComponents/DynamicListView.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class DynamicListView extends StatelessWidget {
+  final List<String> items;
+  DynamicListView({ Key key, @required this.items }):super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dynamic ListView',
+      home: Scaffold(
+        body: new ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          }
+        ),
+      ),
+    );
+  }
+}
